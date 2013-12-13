23a842139fb844fa9f7c85fcff2441b9aa2ca84c
Arno Luebke <arno.luebke@googlemail.com>
2013-12-13 20:54:21 +0900

    fourth commit

--- Src/CLUE/Program.cpp
+++ Src/CLUE/Program.cpp
@@ -52,3 +52,53 @@ void CLUEProgramDestroy(cl_program program)
 	clReleaseProgram(program);
 }
 //-----------------------------------------------------------------------------
+cl_kernel CLUEKernelCreate(cl_program program, const char* name)
+{
+	cl_kernel kernel;
+	cl_int err;	
+
+	clCreateKernel(program, name, &err);
+
+	CLUE_CHECK_CL_ERROR(err, "Could not create kernel");
+
+	return kernel;
+}
+//-----------------------------------------------------------------------------
+void CLUEKernelSetArgument(
+	cl_kernel kernel, 
+	cl_uint argIndex, 
+	size_t size, 
+	const void* argValue
+)
+{
+	cl_int err;
+
+	err = clSetKernelArg(kernel, argIndex, size,  argValue);
+	CLUE_CHECK_CL_ERROR(err, "Could not set kernel argument");
+}
+//-----------------------------------------------------------------------------
+void CLUEKernelEnqueue(
+	cl_kernel kernel, 
+	cl_uint workDim, 
+	const size_t* globalWorkSize,
+	const size_t* localWorkSize
+)
+{
+	clEnqueueNDRangeKernel(
+		CLUEContextGetCLCommandQueue(),
+		kernel,
+		workDim,
+		NULL,
+		globalWorkSize,
+		localWorkSize,
+		0, 
+		NULL, 
+		NULL
+	);
+}
+//-----------------------------------------------------------------------------
+void CLUEKernelDestroy(cl_kernel kernel)
+{
+	CLUE_CHECK_CL_ERROR(clReleaseKernel(kernel), "Could not release kernel.")
+}
+//-----------------------------------------------------------------------------
--- Src/CLUE/Program.h
+++ Src/CLUE/Program.h
@@ -14,4 +14,35 @@ cl_program CLUEProgramCreateWithSource(cl_uint count, const char** strings);
 /// Destroys an OpenCL program.
 void CLUEProgramDestroy(cl_program program);
 
+
+/// Create a kernel from an OpenCL program
+///
+/// \param program OpenCL program to create the kernel from.
+/// \param name Name of the kernel function in the program's source code.
+cl_kernel CLUEKernelCreate(cl_program program, const char* name);
+
+/// Sets the argument for a kernel.
+///
+/// \param kernel Kernel which argument should be set.
+/// \param argIndex Index of the argument.
+/// \param size Size of the argument.
+/// \param argValue Value of the argument.
+void CLUEKernelSetArgument(
+	cl_kernel kernel, 
+	cl_uint argIndex, 
+	size_t size, 
+	const void* argValue
+);
+
+/// Enqeues a kernel to the command queue of the CLUE context.
+void CLUEKernelEnqueue(
+	cl_kernel kernel, 
+	cl_uint workDim, 
+	const size_t* globalWorkSize,
+	const size_t* localWorkSize
+);
+
+/// Destroys a kernel.
+void CLUEKernelDestroy(cl_kernel kernel);
+
 #endif /* end of include guard: PROGRAM_H */
